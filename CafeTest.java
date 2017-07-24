4099748d2e34fa31a8b19be8601ace9e3dc6adea
laurenap159 <laurenap159@gmail.com>
2017-07-24 14:24:40 -0400

    Update CafeTest.java

--- CafeTest.java
+++ CafeTest.java
@@ -10,16 +10,29 @@ public class CafeTest {
 	@Test 
 	public void canBrewEspresso () {
 		
-		//instantiate a cafe object
+		//instantiate a cafe object:: given clause
 		Cafe cafe = new Cafe();
+		cafe.restockBeans(7);
 		
-		//call brew method and pass in coffee type
+		//call brew method and pass in coffee type:: when clause since passing paramter
 		Coffee coffee = cafe.brew(CoffeeType.Espresso);
 		
+		//then clause		
 		//test the input is correct-- make sure it is an espresso, make sure it doesnt have milk, and have enough milk
 		Assert.assertEquals(CoffeeType.Espresso, coffee.getType());
 		Assert.assertEquals (0, coffee.getMilk());
 		Assert.assertEquals (7, coffee.getBeans());
 	}
 	
+	@Test 
+	public void brewingEspressoConsumeBeans() {
+		//given
+		Cafe cafe = new Cafe();
+		cafe.restockBeans(7);
+		//when
+		Coffee coffee = cafe.brew(CoffeeType.Espresso);
+		//then
+		Assert.assertEquals (0, cafe.getBeansInStock());
+	}
+		
 }
